b5f7c98878e4721fc16b3305f626df51cad79445
Prashanth Raghavan <prashanth.raghavan@klarna.com>
2025-06-17 16:34:58 +0200

    Fix duplicate const

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-const { extractEvent } = require('./parser');
-const { addEventToCalendar } = require('./calendar');
-const { scheduleReminder } = require('./reminder');
-
-dotenv.config();
-
-const fs = require('fs');
+dotenv.config();const fs = require('fs');
 if (!fs.existsSync('service_account.json') && process.env.SERVICE_ACCOUNT_JSON_BASE64) {
   const json = Buffer.from(process.env.SERVICE_ACCOUNT_JSON_BASE64, 'base64').toString('utf-8');
   fs.writeFileSync('service_account.json', json);
